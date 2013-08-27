33f3bc3cd594e3668c1a514ad9ad79c808828d9e
Jeremy Gailor <jeremy@infinitecube.com>
2013-08-27 12:04:49 -0700

    Initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brewsdays::Application.config.secret_key_base = 'e9e8de0a25a88011786d7f5e5b61c9c96614c84d3f67ac4965259e04f23b3ee1c3893e6953768d5616fbd4c9516197d5ee5484d7130ae6deced52ea536287465'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brewsdays::Application.config.session_store :cookie_store, key: '_brewsdays_session'
